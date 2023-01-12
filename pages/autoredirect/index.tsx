e95721287972ade2ec9c0fc2678cee72f935f071
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2023-01-12 17:30:22 +0700

    fix: 🐛 change window open to window location href

--- pages/autoredirect/index.tsx
+++ pages/autoredirect/index.tsx
@@ -3,9 +3,9 @@ import { useEffect } from "react"
 const AutoRedirect = () => {
     useEffect(() => {
         if (navigator.appVersion.toLocaleLowerCase().includes("android")) {
-            window.open("https://play.google.com/store/apps/details?id=net.ekgames.fclasshero")
+            window.location.href = "https://play.google.com/store/apps/details?id=net.ekgames.fclasshero"
         } else if (navigator.appVersion.toLocaleLowerCase().includes("iphone")) {
-            window.open("https://apps.apple.com/id/app/roblox/id431946152")
+            window.location.href = "https://apps.apple.com/id/app/roblox/id431946152"
         }
         console.log(navigator.appVersion);
         console.log(navigator.userAgent)
